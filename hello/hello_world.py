42c825fdba97500a726fdef2bec7ba31f42d3e1d
tianliuxin <1627094964@qq.com>
2021-01-21 17:36:18 +0800

    修改dev分支

--- hello/hello_world.py
+++ hello/hello_world.py
@@ -2,7 +2,13 @@ print("hello,world!")
 print("I am from a branch!")
 # print("how merge branch")
 print('修复了一个bug')
+#========================
+print('add dev')
+#========================
 print("how merge branch")
 print("我是从删除的分支里出来的")
 print('test stash')
+#=================
+print('second dev')
+#=================
 print("很不错,修复完bug回来干活了,活干完了,提交!")
